bbde1fe218ac13b971c94f9ce2344507ed2b5d9f
hdDuy0505 <92098917+hdDuy0505@users.noreply.github.com>
2022-12-10 17:47:37 +0700

    Add UI List presentation

--- src/modules/home/pages/MHome.jsx
+++ src/modules/home/pages/MHome.jsx
@@ -1,27 +1,29 @@
 import { Link } from 'react-router-dom'
 
 import group from 'assets/images/group.jpg'
-import poll from 'assets/images/poll.jpg'
+import presentation from 'assets/images/presentation.png'
 
 function MHome() {
     return (
         <div className="flex justify-center pt-20">
             <div className="rounded border bg-white p-5">
-                <h1 className="text-center text-2xl">Start by creating your first Poll</h1>
+                <h1 className="text-center text-2xl">Start by creating your first presentation</h1>
                 <div className="my-5 flex justify-center">
                     <div className="mr-1 flex h-52 flex-[0.5] flex-col items-center justify-between rounded-xl border pb-2 text-center hover:border-gray-400">
                         <img
                             className="h-24 w-full rounded-t-xl object-cover"
-                            src={poll}
-                            alt="poll"
+                            src={presentation}
+                            alt="presentation"
                         />
                         <div>
-                            <h2 className="font-semibold text-gray-800">Realtime Poll</h2>
+                            <h2 className="font-semibold text-gray-800">Presentation</h2>
                             <p className="text-xs text-gray-700">Choose the best option</p>
                         </div>
-                        <button className="rounded bg-blue-600 px-4 py-1 text-sm font-semibold text-white">
-                            Create poll
-                        </button>
+                        <Link to="/presentation/create">
+                            <button className="rounded bg-blue-600 px-4 py-1 text-sm font-semibold text-white">
+                                Create presentation
+                            </button>
+                        </Link>
                     </div>
                     <div className="ml-1 flex h-52 flex-[0.5] flex-col items-center justify-between rounded-xl border pb-2 text-center hover:border-gray-400">
                         <img
--- src/modules/presentation/pages/MPresentationList.jsx
+++ src/modules/presentation/pages/MPresentationList.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import { getAll as getAllGroup } from 'common/queries-fn/groups.query'
+import CLoading from 'common/components/CLoading'
+import { ROLE } from 'common/constant'
+import { FireIcon,EllipsisVerticalIcon } from '@heroicons/react/24/outline'
+import { Link, useNavigate } from 'react-router-dom'
+import { Dropdown } from 'flowbite-react'
+import { PlayIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 function MPresentationList() {
-    return <div>MPresentationList</div>
+    return (
+        <div className="mx-2 pt-10 md:mx-10 lg:mx-20 xl:mx-40 2xl:mx-60">
+            <div className="bg-white p-5">
+                <div className="container grid grid-cols-1 gap-4 p-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                    <div className="overflow-hidden rounded border border-gray-300 bg-white">
+                        <div className="px-6 py-4">
+                            <div className="flex justify-center">
+                                <Link
+                                    className="mb-2 flex-1 text-xl font-bold"
+                                    to={'/presentation/create'}
+                                >
+                                    <div>Name</div>
+                                </Link>
+
+                                <Dropdown
+                                    arrowIcon={false}
+                                    inline={true}
+                                    placement="right"
+                                    label={<EllipsisVerticalIcon className="h-7 w-7" />}
+                                >
+                                    <Link to={'/silde/1'}>
+                                        <Dropdown.Item className="text-[#1A94FF]">
+                                            <PlayIcon className="h-6 w-6 cursor-pointer pr-1 text-[#1A94FF]" />
+                                            <h3>Present</h3>
+                                        </Dropdown.Item>
+                                    </Link>
+
+                                    <Dropdown.Item className="text-red-600">
+                                        <XMarkIcon className="h-6 w-6 cursor-pointer pr-1 text-red-600" />
+                                        <h3>Delete</h3>
+                                    </Dropdown.Item>
+                                </Dropdown>
+                            </div>
+                            <Link to={'/presentation/create'}>
+                                <div className="text-md mb-2 flex items-center">
+                                    <div>
+                                        <FireIcon className="mr-2 h-5 w-5" />
+                                    </div>
+                                    You are owner of this presentation
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default MPresentationList
